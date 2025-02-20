4a2ac30b7e5283641b9bc74788f4cc066df1acc5
Kalana Shehara <sheeykal@gmail.com>
2025-02-21 03:04:00 +0530

    Connected to 3D models from the database

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,7 +8,16 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+{
+  List<Map<String, dynamic>> model3dlist = [
+    {
+      "model3dUrl" : "https://lfgyrawtejihaevuxtvf.supabase.co/storage/v1/object/public/SL%20Tower%203D//SL%20Tower.glb",
+      "photoUrl" : "https://lfgyrawtejihaevuxtvf.supabase.co/storage/v1/object/public/SL%20Tower%20IMG//SL%20Tower.png",
+      "name" : "Sigiriya"
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
