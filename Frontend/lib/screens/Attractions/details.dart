0feb7c9c19a7f2fcc2cf1f4ebe17037888ecd3ac
Chamod Pankaja <152173912+chamodpankaja@users.noreply.github.com>
2025-03-04 18:17:28 +0530

    favourite places part implemented

--- Frontend/lib/screens/Attractions/details.dart
+++ Frontend/lib/screens/Attractions/details.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:serendib_trails/screens/main_screen.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 final String googleApiKey = dotenv.env['GOOGLE_MAPS_API_KEY'] ?? "";
 
@@ -16,9 +18,10 @@ class TripDetailScreen extends StatelessWidget {
 
     return WillPopScope(
       onWillPop: () async {
-        Navigator.pushReplacement(
+        Navigator.pushAndRemoveUntil(
           context,
           MaterialPageRoute(builder: (context) => MainScreen()),
+          (Route<dynamic> route) => false,
         );
         return false;
       },
@@ -33,9 +36,10 @@ class TripDetailScreen extends StatelessWidget {
           leading: IconButton(
             icon: Icon(Icons.arrow_back, color: Colors.white),
             onPressed: () {
-              Navigator.pushReplacement(
+              Navigator.pushAndRemoveUntil(
                 context,
                 MaterialPageRoute(builder: (context) => MainScreen()),
+                (Route<dynamic> route) => false,
               );
             },
           ),
@@ -83,16 +87,39 @@ class TripDetailScreen extends StatelessWidget {
                                   child: Column(
                                     crossAxisAlignment: CrossAxisAlignment.start,
                                     children: [
-                                      // Display the image (if available)
-                                      place["photos"] != null && place["photos"].isNotEmpty
-                                          ? Image.network(
-                                              "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place["photos"][0]["photo_reference"]}&key=$googleApiKey",
-                                              fit: BoxFit.cover,
-                                              height: 180,
-                                              width: double.infinity,
-                                            )
-                                          : Container(), // If no image available, display nothing
-
+                                      Stack(
+                                        children: [
+                                          // Display the image (if available)
+                                          place["photos"] != null && place["photos"].isNotEmpty
+                                              ? Image.network(
+                                                  "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place["photos"][0]["photo_reference"]}&key=$googleApiKey",
+                                                  fit: BoxFit.cover,
+                                                  height: 180,
+                                                  width: double.infinity,
+                                                )
+                                              : Container(
+                                                  height: 180,
+                                                  color: Colors.grey,
+                                                ), // If no image available, display a grey container
+                                          Positioned(
+                                            top: 8,
+                                            right: 8,
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                color: Colors.white.withOpacity(0.8),
+                                                borderRadius: BorderRadius.circular(50),
+                                              ),
+                                              child: IconButton(
+                                                icon: Icon(Icons.bookmark_border, color: Colors.black),
+                                                onPressed: () {
+                                                  // Handle bookmark action
+                                                  _saveFavoritePlace(context, place);
+                                                },
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
                                       Padding(
                                         padding: const EdgeInsets.all(8.0),
                                         child: Column(
@@ -153,6 +180,48 @@ class TripDetailScreen extends StatelessWidget {
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Could not open Google Maps.')),
+      );
+    }
+  }
+
+  Future<void> _saveFavoritePlace(BuildContext context, Map<String, dynamic> place) async {
+    User? user = FirebaseAuth.instance.currentUser;
+    if (user != null) {
+      final userId = user.uid;
+      final placeId = place['place_id'];
+
+      // Check if the place already exists in the favourite_places collection for the current user
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection('favourite_places')
+          .where('userId', isEqualTo: userId)
+          .where('placeId', isEqualTo: placeId)
+          .get();
+
+      if (querySnapshot.docs.isEmpty) {
+        // Place does not exist, add it to the collection
+        await FirebaseFirestore.instance.collection('favourite_places').add({
+          'userId': userId,
+          'placeId': placeId,
+          'name': place['name'],
+          'vicinity': place['vicinity'],
+          'rating': place['rating'],
+          'photoUrl': place['photos'] != null && place['photos'].isNotEmpty
+              ? "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place['photos'][0]['photo_reference']}&key=$googleApiKey"
+              : null,
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Place added to favourites'), backgroundColor: Colors.green),
+        );
+      } else {
+        // Place already exists, show a message or handle accordingly
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Place already added to favourites'), backgroundColor: Colors.orange),
+        );
+        print('Place already added to favourites');
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('User not logged in.')),
       );
     }
   }
