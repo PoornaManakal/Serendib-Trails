c47c71a6ff1abe65313dbaf2c6d184c335b14eaa
Devin2002 <sujeewakuruppuarachchi71@gmail.com>
2025-03-19 15:44:04 +0530

    Update weather-features colors.dart

---
+++ Frontend/lib/screens/Weather_features/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const gradient1 = Color.fromARGB(255, 3, 161, 3);
+const gradient2 = Color.fromARGB(255, 0, 64, 9);
+const gradient3 = Color(0xff000000);
+const secondaryBgColor = Color.fromARGB(59, 0, 118, 53);
+const primaryTextColor = Color.fromARGB(255, 255, 255, 255);
+const secondaryTextColor = Color.fromARGB(200, 160, 156, 161);
+const borderColor1 = Color.fromARGB(105, 255, 255, 255);
+const borderColor2 = Color.fromARGB(156, 51, 0, 64);
