9595b71c77209da03bc55f8c5693f3547a3c47f3
Abiram <abiram.20221694@iit.ac.lk>
2025-03-22 18:39:40 +0530

    Added Texts and aligned them

--- main.dart
+++ main.dart
@@ -7,4 +7,44 @@ void main(){
 class AugmentedReality extends StatelessWidget{
   const AugmentedReality({super.key});
 
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.white,
+      body:SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+           const SizedBox(height:30),
+           const Text(
+            "Historical Landmarks",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+            textAlign: Center,
+           ),
+           const SizedBox(height: 10),
+           const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              "Explore the rich history of famous landmarks and discover their over time.",
+              style: TextStyle(
+                fontSize: 16
+                color: Colors.black
+              ),
+              textAlign: Center,
+            ),)
+          ],  
+        ),
+      )
+    )
+  )
+}
+
+
 }
